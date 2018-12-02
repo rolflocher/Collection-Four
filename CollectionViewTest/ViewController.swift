89f7d664c029777ede0d70b678d8f2a8b99c3b94
Rolf Locher <rolflocher@Rolfs-MacBook-Pro.local>
2018-12-01 19:05:47 -0500

    Initial Commit

---
+++ CollectionViewTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionViewTest
+//
+//  Created by Rolf Locher on 12/1/18.
+//  Copyright © 2018 Rolf Locher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
